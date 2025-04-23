65dd7cc4d06d15979cf28830f153497447efe4b8
Mon-Rico <115199569+Mon-Rico@users.noreply.github.com>
2025-04-23 00:37:35 -0700

    Debugging names

--- models/BankModel.js
+++ models/BankModel.js
@@ -46,7 +46,7 @@ async function getBank(body) {
 async function createBank(BankData) {
     const [Name, Addr, Money] = Object.values(BankData);
     await db.run(
-        'INSERT INTO customer(Name, Address, Money) VALUES(?,?,?)',
+        'INSERT INTO bank(Name, Address, Money) VALUES(?,?,?)',
         [Name, Addr, Money]
     );
 }
