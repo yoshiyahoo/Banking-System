f0013aa4593a9f701025bc196bd2a446d221b508
Mon-Rico <115199569+Mon-Rico@users.noreply.github.com>
2025-04-22 23:17:56 -0700

    changed mysql password to Monica's device

--- models/BankModel.js
+++ models/BankModel.js
@@ -8,18 +8,31 @@ async function getBanks(){
 
 
 //get one Bank by ID
+<<<<<<< Updated upstream
 async function getBank(){
     const rows = await db.run('SELECT * FROM bank WHERE Bank_ID == ?',[Bank_ID]);
+=======
+async function getBank() {
+    const rows = await db.run('SELECT * FROM bank WHERE Bank_ID == ?',[BankID]);
+>>>>>>> Stashed changes
     return rows[0];
 }
 
 //create a new bank row
+<<<<<<< Updated upstream
 //What is Bank.Money?
 async function createBank(){
     const {Bank_ID, Name, Addr,Money } = BankData;
     await db.run(
         'INSERT INTO bank(Bank_ID, Name, Addr,Money ) VALUES(?,?,?,?)',
         [Bank_ID, Name, Addr,Money ]
+=======
+async function createBank(BankData) {
+    const {BankID, Name, Addr, Money} = BankData;
+    await db.run(
+        'INSERT INTO customer(BankID, Name, Addr,Money ) VALUES(?,?,?,?)',
+        [BankID, Name, Addr, Money]
+>>>>>>> Stashed changes
     );
 }
 
--- models/db.js
+++ models/db.js
@@ -7,7 +7,7 @@ class DataBase {
         this.connection = mysql.createConnection({
             host: '127.0.0.1', // Your database host
             user: 'root', // Your MySQL username
-            password: '082102', // Your MySQL password
+            password: 'new_password', // Your MySQL password
             port: '3306', // Your MySQL port, normally "3306"
             database: 'test', // DO NOT EDIT (You need to create a "test" database from your "MySQL Workbench")
         });
